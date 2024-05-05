c456a6433200d472c98ad232c7beb268de0e52d2
Jakob <minenash@protonmail.com>
2024-05-05 18:55:08 -0400

    Created mod

--- src/main/java/dev/jakobt/code_effect/CodeEffect.java
+++ src/main/java/dev/jakobt/code_effect/CodeEffect.java
@@ -1,4 +1,54 @@
-package dev.jakobt.codeeffect;
+package dev.jakobt.code_effect;
 
-public class CodeEffect {
+import com.mojang.datafixers.util.Either;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.component.EnchantmentEffectComponentTypes;
+import net.minecraft.component.type.AttributeModifierSlot;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentEffectContext;
+import net.minecraft.enchantment.EnchantmentLevelBasedValueType;
+import net.minecraft.enchantment.effect.value.AddEnchantmentEffectType;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
+import net.minecraft.loot.condition.DamageSourcePropertiesLootCondition;
+import net.minecraft.predicate.TagPredicate;
+import net.minecraft.predicate.entity.DamageSourcePredicate;
+import net.minecraft.registry.*;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.entry.RegistryEntryList;
+import net.minecraft.registry.tag.DamageTypeTags;
+import net.minecraft.registry.tag.EnchantmentTags;
+import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CodeEffect implements ModInitializer {
+
+    private static final Map<Identifier, CodeConsumer> registry = new HashMap<>();
+    public static void register(Identifier hook, CodeConsumer code) {
+        registry.put(hook, code);
+    }
+    public static CodeConsumer get(Identifier hook) {
+       return registry.get(hook);
+    }
+
+    @FunctionalInterface
+    public interface CodeConsumer {
+        void run(ServerWorld world, int level, EnchantmentEffectContext context, Entity user, Vec3d pos);
+    }
+
+    @Override
+    public void onInitialize() {
+        Registry.register(Registries.ENCHANTMENT_ENTITY_EFFECT_TYPE, new Identifier("code_effect", "run_code"), RunCodeEnchantmentEffectType.CODEC);
+
+        register(new Identifier("code_effect", "testhook"), (world, level, context, user, pos) -> {
+            System.out.println( user.getNameForScoreboard() + "→" + level );
+        });
+    }
+
 }
--- src/main/java/dev/jakobt/code_effect/RunCodeEnchantmentEffectType.java
+++ src/main/java/dev/jakobt/code_effect/RunCodeEnchantmentEffectType.java
@@ -1,4 +1,32 @@
 package dev.jakobt.code_effect;
 
-public class RunCodeEnchantmentEffectType {
+import com.mojang.logging.LogUtils;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.enchantment.EnchantmentEffectContext;
+import net.minecraft.enchantment.effect.EnchantmentEntityEffectType;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Vec3d;
+import org.slf4j.Logger;
+
+public record RunCodeEnchantmentEffectType(Identifier hook) implements EnchantmentEntityEffectType {
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final MapCodec<RunCodeEnchantmentEffectType> CODEC = RecordCodecBuilder.mapCodec(
+            instance -> instance.group(Identifier.CODEC.fieldOf("hook").forGetter(RunCodeEnchantmentEffectType::hook))
+                    .apply(instance, RunCodeEnchantmentEffectType::new)
+    );
+    @Override
+    public MapCodec<RunCodeEnchantmentEffectType> getCodec() { return CODEC; }
+
+    @Override
+    public void apply(ServerWorld world, int level, EnchantmentEffectContext context, Entity user, Vec3d pos) {
+        CodeEffect.CodeConsumer consumer = CodeEffect.get(hook);
+        if (consumer != null)
+            consumer.run(world, level, context, user, pos);
+        else
+            LOGGER.error("Enchantment run_code effect failed for non-existent hook {}", this.hook);
+    }
+
 }
