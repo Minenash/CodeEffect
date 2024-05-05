a614fbd95ac8e6c876f8b6b576b57bb02629a1d1
Jakob <minenash@protonmail.com>
2024-05-05 19:19:04 -0400

    Removed imports

--- src/main/java/dev/jakobt/code_effect/CodeEffect.java
+++ src/main/java/dev/jakobt/code_effect/CodeEffect.java
@@ -1,25 +1,9 @@
 package dev.jakobt.code_effect;
 
-import com.mojang.datafixers.util.Either;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.component.EnchantmentEffectComponentTypes;
-import net.minecraft.component.type.AttributeModifierSlot;
-import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentEffectContext;
-import net.minecraft.enchantment.EnchantmentLevelBasedValueType;
-import net.minecraft.enchantment.effect.value.AddEnchantmentEffectType;
 import net.minecraft.entity.Entity;
-import net.minecraft.item.Item;
-import net.minecraft.loot.condition.DamageSourcePropertiesLootCondition;
-import net.minecraft.predicate.TagPredicate;
-import net.minecraft.predicate.entity.DamageSourcePredicate;
 import net.minecraft.registry.*;
-import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.registry.entry.RegistryEntryList;
-import net.minecraft.registry.tag.DamageTypeTags;
-import net.minecraft.registry.tag.EnchantmentTags;
-import net.minecraft.registry.tag.ItemTags;
-import net.minecraft.registry.tag.TagKey;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3d;
